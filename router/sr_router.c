240d19d2e7ba40ba8ea632fcc7b493531f5ce049
jneighbs <jneighbs@stanford.edu>
2012-11-12 21:40:37 -0800

    asdf

--- router/sr_router.c
+++ router/sr_router.c
@@ -590,7 +590,7 @@ int receiveValidEchoRequest(sr_icmp_hdr_t* icmpheader, unsigned int len){
 
 void ipToMe(struct sr_instance* sr, sr_ip_hdr_t* ipheader, char* interfaceName){
 	printf("--function: ipToMe-- \n");
-	unsigned int len = ntohl(ipheader->ip_len) - sizeof(*ipheader);
+	unsigned int len = ntohl(ipheader->ip_len) - sizeof(sr_ip_hdr_t);
 	printf("len: %i\n", len);
 	
 	if(ipheader->ip_p==ip_protocol_icmp){ 
